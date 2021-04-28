e0707e793528174144dea052e472847d3884b0fd
iam source <>
2021-04-28 01:06:54 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='flatter',
+      version='0.1',
+      description='can flatten nested array',
+      url='https://github.com/iamsource2021/Flatter.git',
+      author='Eduardo Ortiz',
+      author_email='eduardooa1980@gmail.com',
+      license='MIT',
+      include_package_data=True,
+      install_requires=['underscore.py'],
+      zip_safe=False)
